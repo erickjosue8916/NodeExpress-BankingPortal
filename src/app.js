9d0cc0b249a354e3d8c7e9e2498a5ae22f4438ba
erickjosue8916 <erickjosue8916@gmail.com>
2020-01-06 18:58:58 -0600

    Edited module 1

--- src/app.js
+++ src/app.js
@@ -15,4 +15,4 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(3000, console.log('PS Proyect Running on port 3000!'));
+app.listen(3000, () => console.log('PS Proyect Running on port 3000!'));
